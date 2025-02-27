a2aa71bfdccdb900673eb02d75f9dc7a724db1d2
NickTrip27 <ttriplett27@gcds.net>
2025-02-27 09:53:36 -0500

    Delete CS1 & CS2/CS2/Nicholas_Triplett_fixed_length_conversion.py

--- CS1 & CS2/CS2/Nicholas_Triplett_fixed_length_conversion.py
+++
@@ -1,5 +0,0 @@
-fhand = open('student_data_cs2.txt')
-with open('dic.csv', 'w') as fout:
-    for line in fhand:
-        fout.write(line[0:4].rstrip() + "," + line[5:19].rstrip() + "," + line[21:35].rstrip() + "," + line[36:42].rstrip() + "," + line[42:46].rstrip() + "," + line[47:58].rstrip() + "," + line[59:66].rstrip() + "," + line[67:73].rstrip() + "," + line[76:86].rstrip() + "," + line[86:87].rstrip() + "," + line[93:102].rstrip() + "," + line[102:112].rstrip() + "\n")
-    
